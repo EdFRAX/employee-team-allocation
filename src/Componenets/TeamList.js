4c135f61722e8f2da4eabe3eb7783b19bf98838b
EdFRAX <lamprawat3@gmail.com>
2023-09-30 10:44:10 +0530

    Update TeamList.js

--- src/Componenets/TeamList.js
+++ src/Componenets/TeamList.js
@@ -1,7 +1,7 @@
 const TeamList = ({selectedTeam, handleChangeTeam}) => {
     return(
         <div className="conatainer mb-3">
-            <div className="row justify-content-center">
+            <div className="row justify-content-center mx-0">
                 <div className="col-10">
                     <select className="form-select form-select-lg" value={selectedTeam} onChange={handleChangeTeam}>
                         <option value="TeamA">TeamA</option>
